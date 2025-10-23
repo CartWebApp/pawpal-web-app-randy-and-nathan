f307e0d1b34b3b1759f7ec50cac8884a505fe061
Dafreeman794 <nategcook@gmail.com>
2025-10-23 08:03:39 -0700

    im attempting to add an account feature

---
+++ Pal.js
@@ -0,0 +1,5 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => console.log(userCredential.user))
+  .catch((error) => console.error(error.message));
